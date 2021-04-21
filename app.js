0bb19b0f00821506efe4bdcb1f42a4406d2ae7b2
Agata-Bernat <agata.bernat.art@gmail.com>
2021-04-21 13:51:22 +0200

    First task ist ready.

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-/** EXTERNAL DEPENDENCIES */
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-/** ROUTERS */
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-/** INIT */
 const app = express();
 
-/** LOGGING */
-app.use(logger('dev'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-/** REQUEST PARSERS */
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-/** STATIC FILES*/
 app.use(express.static(path.join(__dirname, 'public')));
 
-/** ROUTES */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/** EXPORT PATH */
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
